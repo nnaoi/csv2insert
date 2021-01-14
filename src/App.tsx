a3e9c9564c979db8456cee2e19710c9dc75259b6
nnaoi <naoinamiki1217@gmail.com>
2021-01-15 08:58:59 +0900

    wip: csv2insert

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import { Box, Button, Card, CardActions, CardContent, Checkbox, Fab, FormControlLabel, FormGroup, Switch, TextField } from '@material-ui/core';
 import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  const [csvText, setCsvText] = useState('');
+  const [insertText, setInsertText] = useState('');
+  const [isCopyChecked, setCopyChecked] = useState(true);
+
+  const csvToInert = () => {
+    const textWithExtraTabsRemoved = csvText.replace(/\t+?\r?\n/g, '\r\n');
+  };
+
   return (
     <div className="App">
       <Card>
@@ -15,13 +23,23 @@ function App() {
             defaultValue="Default Value"
             variant="filled"
             fullWidth
+            value={csvText}
+            onBlur={(e: React.FocusEvent<HTMLInputElement>) => setCsvText(e.target.value) }
           />
           <Box m={1} textAlign="center">
-              <Button color="primary" variant="contained">
+              <Button color="primary" 
+                variant="contained"
+                onClick={() => csvToInert()}
+              >
                 <KeyboardArrowDownIcon fontSize="large" />
               </Button>
               <FormControlLabel
-                control={<Switch />}
+                control={
+                  <Switch 
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => 
+                      setCopyChecked(e.target.checked)} 
+                    checked={isCopyChecked} 
+                  />}
                 label="クリップボードにコピー"
                 style={{ marginLeft: "4px" }}
               />
@@ -36,10 +54,6 @@ function App() {
             fullWidth
           />
         </CardContent>
-        <CardActions>
-          
-          <Checkbox />
-        </CardActions>
       </Card>
     </div>
   );
