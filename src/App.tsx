efe8a74077de10754159f15eb59fcec57b149f16
nnaoi <naoinamiki1217@gmail.com>
2021-01-16 14:03:20 +0900

    Changed to use papaparse for parsing.
    Because it may not work properly when the input data contains line breaks.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
-import { Box, Button, Card, CardContent, FormControlLabel, Switch, TextField } from '@material-ui/core';
-import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
 import React, { useState } from 'react';
+
+import { 
+  Box, 
+  Button, 
+  Card, 
+  CardContent, 
+  FormControlLabel, 
+  Switch, 
+  TextField 
+} from '@material-ui/core';
+import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
+import { readString } from 'react-papaparse';
+import PapaParse from 'papaparse';
 
 function App() {
   const [csvText, setCsvText] = useState('');
@@ -8,34 +19,30 @@ function App() {
   const [isCopyChecked, setCopyChecked] = useState(true);
 
   const csvToInert = () => {
-    const textWithExtraTabsRemoved = csvText.replace(/\t+?\r?\n/g, '\r\n');
-    const rows = textWithExtraTabsRemoved
-      .split('\r\n')
-      .map(r => r.split('\t'));
-    let tableName: string;
-    let tableNameIndex = 0;
-    let columns: string[];
-    let columnsIndex = 1;
-    let insertText = "";
-    rows.forEach((r, i) => {
-      if (i === tableNameIndex) {
-        tableName = r[0];
-        columnsIndex = i + 1;
-        insertText += `-- ${tableName}\r\n`;
-      } else if (i === columnsIndex) {
-        columns = r;
-      } else if (r.length === 1 && !r[0]) {
-        insertText += "GO\r\n\r\n";
-        tableNameIndex = i + 1;
-      } else {
-        const rowWithQuotes = r
-          .map(t => t === 'NULL' ? 'NULL' : `'${t}'`);
-        insertText += `INSERT INTO ${tableName}(${columns.join(', ')}) VALUES (${rowWithQuotes.join(',')});\r\n`;
-      }
+    const tables = csvText.replace(/\t+?\r?\n/g, '\n')
+      .split(/^\r?\n/m);
+    let sqlStateMent = '';
+    tables.forEach(table => {
+      const regex = /^.+?\r?\n/;
+      const tableName = table.match(regex)
+        ?.find(tn => tn)
+        ?.replace(/\r?\n/, '');
+      table = table.replace(regex, '').replace(/\r?\n\t*?$/, '');
+      sqlStateMent += `-- ${tableName}\n`;
+
+      const result = readString(table, { header: true }) as 
+        PapaParse.ParseResult<{[column: string]: string}>;
+      result.data.forEach(row => {
+        const columns = Object.keys(row);
+        const values = Object.values(row)
+          .map(v => v === 'NULL' ? 'NULL' : `'${v}'`)
+          sqlStateMent += `INSERT INTO ${tableName}(${columns.join(', ')}) VALUES (${values.join(', ')});\n`;
+      });
+
+      sqlStateMent += 'GO\n\n';
     });
-  
-    insertText += 'GO'
-    setInsertText(insertText);
+
+    setInsertText(sqlStateMent);
   };
 
   return (
